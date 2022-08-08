25876654eb30acbc5465f1b1420e6edf27e340f9
viniciusvdias <vvsdias@gmail.com>
2022-08-08 19:15:33 -0300

    update

--- src/bash/visualize-from-pattern-dir.sh
+++ src/bash/visualize-from-pattern-dir.sh
@@ -2,6 +2,9 @@
 
 patterndir=$1
 
+lab="\"$(basename $patterndir)\""
+echo $lab
+
 cat $patterndir/edges \
-   | awk 'BEGIN {g=""} {if (NR == 1) {g=$1"--"$2} else {g=g";"$1"--"$2}} END {print "graph{"g"}"}' \
+   | awk -v lab=$lab 'BEGIN {g=""} {if (NR == 1) {g=$1"--"$2} else {g=g";"$1"--"$2}} END {print "graph{label="lab";"g"}"}' \
    | sfdp -Tx11
